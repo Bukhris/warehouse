702746ef4352acac7eb52a39bad73be38bc8989d
Bukhris <60074481+Bukhris@users.noreply.github.com>
2021-03-02 22:46:51 +0100

    proper DB scanning

--- main.go
+++ main.go
@@ -5,17 +5,28 @@ import	(
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
-	"io"
-		)
-
-var (
-	finalserve string
-	forumid string
-	name string
-	parentid string
+	"fmt"
 		)
 
 func main() {
+
+	
+http.HandleFunc("/login",login)
+http.HandleFunc("/",index)
+http.HandleFunc("/register",register)
+//http.HandleFunc("/serve",serveforum)
+
+log.Fatal(http.ListenAndServeTLS(":9000","cert.pem","key.unencrypted.pem",nil))
+
+}
+
+func index(res http.ResponseWriter, req *http.Request){
+	var (
+		forumid string
+		name string
+		parentid string
+		)
+
 
 	db, err := sql.Open("sqlite3", "./sample.db")
 	log.Println("website.db opened.")
@@ -23,36 +34,27 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
-	rows, err := db.Query(`select * from FORUM where parentid = "none"; ` )
+	rows, err := db.Query(`select * from FORUM ; ` )
 		if err != nil {
 			log.Fatal(err)
 		}
 	defer rows.Close()
 
-	
+	names :="" 
 	for rows.Next() {
 		err := rows.Scan(&forumid, &name,&parentid)
-		if err != nil {
-			log.Fatal(err)
-		}
+		log.Println(forumid,"===",name,"===",parentid)
+		names=names+name+"<br>"
+		if err != nil {log.Fatal(err)}
 	}
-finalserve=`<!DOCTYPE html><html>
+
+	
+finalserve:=`<!DOCTYPE html><html>
     <head>
         <title>WAREHOUSE 11</title>
     </head>
-    <body>`+name+`</body></html>`
-	
-	log.Println(finalserve)
-http.HandleFunc("/login",login)
-http.HandleFunc("/",index)
-http.HandleFunc("/register",register)
-http.HandleFunc("/serve",serveforum)
-
-http.ListenAndServeTLS(":9000","cert.pem","key.unencrypted.pem",nil)
-
-}
+    <body>`+names+`</body></html>`
+	fmt.Fprintf(res,finalserve)
 
 
-func index(res http.ResponseWriter, req *http.Request){
-	io.WriteString(res,finalserve)
 	}
